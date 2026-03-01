3b4402b9b92456fea1faef6c479b5e204a4fb10d
MightBMay <123502487+MightBMay@users.noreply.github.com>
2026-03-01 15:52:39 -0500

    fixed dock issue
    forgot that ontriggerexit does not get called if you manually set player position outside the trigger

--- GDW5_RaftGame/Assets/Trigger_Disembark.cs
+++ GDW5_RaftGame/Assets/Trigger_Disembark.cs
@@ -2,31 +2,38 @@ using UnityEngine;
 
 public class Trigger_Disembark : MonoBehaviour
 {
-    int inTrigger = 0; // int that counts how many players are in the trigger, as a simple bool check would have issues with multiple players.
+    bool inTrigger = false; // int that counts how many players are in the trigger, as a simple bool check would have issues with multiple players.
 
     bool isOnBoat = true; // start on boat.
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
 
-        if (other.CompareTag("Player")) inTrigger++;
+        inTrigger = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player")) inTrigger--; 
+        inTrigger = false;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F) && inTrigger >0) Disembark();
+        if (Input.GetKeyDown(KeyCode.F) ) Disembark();
 
     }
 
     void Disembark()
     {
         isOnBoat = !isOnBoat; // togle on boat status, then move.
-        FindFirstObjectByType<PlayerMovement>().transform.position = isOnBoat ? BoatController.instance.RespawnPosition.position : transform.position;
+        Transform player = FindFirstObjectByType<PlayerMovement>().transform;
+        if (!isOnBoat)
+        {
+            player.position = BoatController.instance.transform.position;
+            inTrigger = false;
+        }
+        else
+        player.position = transform.position;
     }
 
 }
