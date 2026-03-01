23f6625d5e97e0128862932245030839bc7c17cb
Zector1114 <zectorssector@gmail.com>
2026-03-01 13:50:02 -0500

    HUD stuff added

--- GDW5_RaftGame/Assets/Scripts/Player/Trigger_SailControl.cs
+++ GDW5_RaftGame/Assets/Scripts/Player/Trigger_SailControl.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Trigger_SailControl : MonoBehaviour
 {
-    [SerializeField]int playersInside = 0;
+    [SerializeField] int playersInside = 0;
     //[SerializeField]PlayerMovement pmove; // current working playerMovement script.
-    [SerializeField]PlayerMovement pmove; // current working playerMovement script.
+    [SerializeField] PlayerMovement pmove; // current working playerMovement script.
 
     [SerializeField] bool isPlayerControlling = false;
 
-    [SerializeField]UI_BoatControl boatControllUI;
+    [SerializeField] UI_BoatControl boatControllUI;
 
     [SerializeField] CameraFollow cFollow;
+
+    bool tutorialDone = false;
+
     private void OnTriggerEnter(Collider collision)
     {
         if (!collision.CompareTag("Player")) return;
@@ -20,7 +23,11 @@ public class Trigger_SailControl : MonoBehaviour
         // pmove = collision.GetComponentInChildren<PlayerMovement>();
         pmove = collision.GetComponentInChildren<PlayerMovement>();
 
-        boatControllUI.SetEnterBoatControl(true);
+        if (!tutorialDone)
+        {
+            boatControllUI.SetUIImage(true);
+            boatControllUI.SetEnterBoatControl(true);
+        }
     }
 
     private void Update()
@@ -28,7 +35,6 @@ public class Trigger_SailControl : MonoBehaviour
         if (pmove == null || playersInside <= 0) return;
 
         //DEBUG : rn if both players entered this trigger, and one of them pressed E/esc, it would effect whoever entered the trigger most recently.
-
 
         // DEBUG : REPLACE WITH INPUT SYSTEM
         if (!isPlayerControlling && Input.GetKeyDown(KeyCode.E))
@@ -43,7 +49,6 @@ public class Trigger_SailControl : MonoBehaviour
             cFollow.ToggleCamera();
         }
     }
-
 
     private void OnTriggerExit(Collider other)
     {
@@ -60,8 +65,12 @@ public class Trigger_SailControl : MonoBehaviour
         pmove.transform.SetParent(transform.root);
         pmove.RB.isKinematic = true;
         pmove.Collider.enabled = false;
-        boatControllUI.SetExitBoatControl(true);
-        boatControllUI.SetEnterBoatControl(false);
+
+        if (!tutorialDone)
+        {
+            boatControllUI.SetExitBoatControl(true);
+            boatControllUI.SetEnterBoatControl(false);
+        }
     }
 
     public void ExitControl()
@@ -75,10 +84,13 @@ public class Trigger_SailControl : MonoBehaviour
         pmove.Collider.enabled = true;
 
         boatControllUI.SetExitBoatControl(false);
+        boatControllUI.SetUIImage(false);
+
+        tutorialDone = true;
 
         if (playersInside <= 0)
             pmove = null;
-        else
-            boatControllUI.SetEnterBoatControl(true);
+        //else
+            //boatControllUI.SetEnterBoatControl(true);
     }
 }
--- GDW5_RaftGame/Assets/Scripts/UI/UI_BoatControl.cs
+++ GDW5_RaftGame/Assets/Scripts/UI/UI_BoatControl.cs
@@ -4,9 +4,11 @@ public class UI_BoatControl : MonoBehaviour
 {
     [SerializeField] GameObject EnterBoatControl;
     [SerializeField] GameObject ExitBoatControl;
-
+    [SerializeField] GameObject DockBoatControl;
+    [SerializeField] GameObject uiImage;
 
     public void SetEnterBoatControl(bool value) => EnterBoatControl.SetActive(value);
     public void SetExitBoatControl(bool value) => ExitBoatControl.SetActive(value);
-
+    public void SetDockBoatControl(bool value) => DockBoatControl.SetActive(value);
+    public void SetUIImage(bool value) => uiImage.SetActive(value);
 }
